190831b898509acc53267034646db82a675cb07b
AndyLiang1 <andyliang1000@gmail.com>
2022-07-26 21:08:50 -0700

    Min is now 0.01

--- src/components/MealList/FormsAndModals/AddFoodFormValidationSchema.ts
+++ src/components/MealList/FormsAndModals/AddFoodFormValidationSchema.ts
@@ -5,36 +5,36 @@ const errorMinValGTOne = 'Please enter a number that is greater than 0';
 
 export const mealListFood_createExisting_Schema = Yup.object({
     existingFoodName: Yup.string().required(errorRequired),
-    existingFoodActualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne)
+    existingFoodActualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 });
 
 export const mealListFood_newNoIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne)
+    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 });
 
 export const mealListFood_newYesIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne)
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 });
 
 export const foodList_newNoIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
+    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
 });
 
 export const foodList_newYesIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.1, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
 });
 
--- src/components/MealList/FormsAndModals/EditFoodFormValidationSchema.ts
+++ src/components/MealList/FormsAndModals/EditFoodFormValidationSchema.ts
@@ -2,39 +2,39 @@ import * as Yup from 'yup';
 
 const errorShouldBeNumber = 'Please enter a number';
 const errorRequired = 'This field is required';
-const errorMinValGTOne = 'Please enter a number that is greater than 0';
+const errorMinValGTOne = 'Please enter a number that is greater than 0.01';
 
 export const mealListFood_editActualAmount_Schema = Yup.object({
-    newActualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne)
+    newActualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 });
 
 export const mealListFood_editReplaceNewNoIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne)
+    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 })
 
 export const mealListFood_editReplaceNewYesIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne)
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    actualAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne)
 })
 
 export const foodList_editReplaceNewNoIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
+    calories: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    proteins: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    carbs: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    fats: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
 });
 
 export const foodList_editReplaceNewYesIng_Schema = Yup.object({
     name: Yup.string().required(errorRequired),
-    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(1, errorMinValGTOne),
+    givenAmount: Yup.number().typeError(errorShouldBeNumber).required(errorRequired).min(0.01, errorMinValGTOne),
 })
 
